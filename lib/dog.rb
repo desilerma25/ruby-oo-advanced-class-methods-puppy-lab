129d3e1734e8865b1ba46ed7edfe01c14e269354
Desiree Lerma <desi.lerma96@yahoo.com>
2020-11-04 01:33:11 -0600

    commit tree 59038cbc00ff48b2dc526463576da4ab6fd112f4

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
-# Add your code here
+class Dog
+    attr_accessor :name, :save
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @save = save # updated here to initialize save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+        puts @@all.map {|dog| dog.name} 
+        # puts (prints) @@all, .map (returns NEW array), enumerate through each dog to get the name.
+    end
+
+    def save # refactor, extract & create it's own method
+        @@all << self
+    end
+end
